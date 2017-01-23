cf908c9c3199af5b8e635972e8d29c76525de6ce
alfredlau <jian.liu@icloud.com>
2017-01-23 14:29:14 +0800

    添加了产品部分

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -42,6 +42,95 @@
             <span class="content">更新了整体架构和首页导航</span>
             <span class="more">更多更新值得期待</span>
         </section>
+        <section class="product">
+            <h1 class="title">
+                项目列表
+                <span class="sub-title">不念过往</span>
+                <span class="sub-title">不畏将来</span>
+            </h1>
+            <div class="content">
+                <div class="item">
+                    <h3><em>准备中</em></h3>
+                    <div class="info">
+                        <p>实现功能数
+                            <span class="count">1<i>项</i></span>
+                        </p>
+                        <p>代码量
+                            <span class="count">1330<i>行</i></span>
+                        </p>
+                    </div>
+                    <div class="doc">
+                        <a>
+                            查看设计文档
+                        </a>
+                    </div>
+                </div>
+                <div class="item">
+                    <h3><em>准备中</em></h3>
+                    <div class="info">
+                        <p>实现功能数
+                            <span class="count">1<i>项</i></span>
+                        </p>
+                        <p>代码量
+                            <span class="count">1330<i>行</i></span>
+                        </p>
+                    </div>
+                    <div class="doc">
+                        <a>
+                            查看设计文档
+                        </a>
+                    </div>
+                </div>
+                <div class="item">
+                    <h3><em>准备中</em></h3>
+                    <div class="info">
+                        <p>实现功能数
+                            <span class="count">1<i>项</i></span>
+                        </p>
+                        <p>代码量
+                            <span class="count">1330<i>行</i></span>
+                        </p>
+                    </div>
+                    <div class="doc">
+                        <a>
+                            查看设计文档
+                        </a>
+                    </div>
+                </div>
+                <div class="item">
+                    <h3><em>准备中</em></h3>
+                    <div class="info">
+                        <p>实现功能数
+                            <span class="count">1<i>项</i></span>
+                        </p>
+                        <p>代码量
+                            <span class="count">1330<i>行</i></span>
+                        </p>
+                    </div>
+                    <div class="doc">
+                        <a>
+                            查看设计文档
+                        </a>
+                    </div>
+                </div>
+                <div class="item">
+                    <h3><em>准备中</em></h3>
+                    <div class="info">
+                        <p>实现功能数
+                            <span class="count">1<i>项</i></span>
+                        </p>
+                        <p>代码量
+                            <span class="count">1330<i>行</i></span>
+                        </p>
+                    </div>
+                    <div class="doc">
+                        <a>
+                            查看设计文档
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -91,11 +180,12 @@
             line-height: 3rem;
             background-color: #fff;
             box-shadow: 0 0 4px #ccc;
-            padding: 5px 15px
+            padding: 5px 0
             .date, .content, .more
                 display: inline-block
             .date::before
-                content: '最新更新: \00a0\00a0'
+                /*\00a0为空格的意思*/
+                content: '\00a0\00a0 最新更新: \00a0\00a0'
                 color: #aaa
             .date
                 color: #999;
@@ -106,6 +196,86 @@
                 right: 2rem
                 color: #f90
 
+        .product
+            width: 90%
+            margin: 2rem auto
+            font-size: 1.4rem;
+            background-color: #f5f9fa;
+            border-top: 2px solid #f90;
+            box-shadow: 0 0 4px #ccc
+            .title
+                display: inline-block
+                width: 20%;
+                margin-top: 3rem;
+                text-align: center;
+                font-size: 2.4rem;
+                font-weight: normal;
+                color: #635c51;
+                .sub-title
+                    display: block;
+                    margin-top: 1rem;
+                    font-size: 1.6rem;
+                    &:first-child
+                        margin-top: 4rem;
+            .content
+                display: inline-block
+                width: 79.5%;
+                border-left: 1px solid #eee;
+                background-color: #fff;
+                .item
+                    display: block
+                    width: 100%
+                    h3
+                        display: inline-block
+                        width: 30%;
+                        margin: 1.5rem 0;
+                        font-size: 1.4rem;
+                        font-weight: normal;
+                        text-align: center;
+                        em
+                            margin-right: 0.5rem;
+                            font-size: 3.6rem;
+                            font-style: normal;
+                            color: #8b999d;
+                    .info
+                        display: inline-block
+                        width: 48%;
+                        padding: 1rem;
+                        border-left: 1px solid #eee;
+                        border-right: 1px solid #eee;
+                        font-size: 14px;
+                        p
+                            display: inline-block;
+                            text-align: center;
+                            width: 56%;
+                            .count
+                                display: block;
+                                margin-top: 5px;
+                                font-size: 2.4rem;
+                                color: #f90;
+                                i
+                                    font-size: 1.2rem;
+                                    font-style: normal;
+                                    color: #999;
+                        p:last-child
+                            width: 40%;
+                    .doc
+                        display: inline-block
+                        width: 15%
+                        padding: 1rem;
+                        a
+                            display: block;
+                            width: 90%;
+                            margin: 1.5rem auto;
+                            max-width: 12rem;
+                            line-height: 4rem;
+                            text-align: center;
+                            text-decoration: none;
+                            color: #fff;
+                            background-color: #f90;
+                            border-radius: 5px;
 
+                .item + .item
+                    border-top: 1px solid #eee;
 </style>
 
